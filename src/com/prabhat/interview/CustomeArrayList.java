aeca43417ca0c922f81c8bd1389317659600a1a7
Prabhat11 <prabhat11.ranjan@gmail.com>
2018-03-01 15:13:35 +0530

    un-neccessary code removed

--- src/com/prabhat/interview/CustomeArrayList.java
+++ src/com/prabhat/interview/CustomeArrayList.java
@@ -1,14 +1,6 @@
 package com.prabhat.interview;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class CustomeArrayList {
-	Set<> set=new HashSet<>();
-	
-	public void setData(){
-		
-	}
 	
 
 }
