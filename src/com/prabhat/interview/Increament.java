051eedf91d6b8f2daf83a0dd1bdbfe1fdfc319a9
Prabhat11 <prabhat11.ranjan@gmail.com>
2018-03-01 12:08:07 +0530

    formatted the code

--- src/com/prabhat/interview/Increament.java
+++ src/com/prabhat/interview/Increament.java
@@ -1,18 +1,22 @@
 package com.prabhat.interview;
 
+/**
+ * 
+ * @author Prabhat
+ *
+ */
 public class Increament {
-	
-	public void preIncreament(){
-		int i=0;
+	public void preIncreament() {
+		int i = 0;
 		i++;
 		System.out.println(i);
-		i=++i;
+		i = ++i;
 		System.out.println(i);
 
 	}
-   public static void main(String[] args) {
-	Increament t=new Increament();
-	t.preIncreament();
+
+	public static void main(String[] args) {
+		Increament t = new Increament();
+		t.preIncreament();
 	}
 }
-   
--- src/com/prabhat/interview/TestTryCase.java
+++ src/com/prabhat/interview/TestTryCase.java
@@ -1,19 +1,24 @@
 package com.prabhat.interview;
 
+/**
+ * 
+ * @author Prabhat
+ *
+ */
 public class TestTryCase {
-	
+
 	public static void main(String[] args) {
-		
-		TestTryCase t=new TestTryCase();
+
+		TestTryCase t = new TestTryCase();
 		System.out.println(t.getNum());
 	}
-	
-	public int getNum(){
-		try{
+
+	public int getNum() {
+		try {
 			return 1;
-		}catch(Exception e){
+		} catch (Exception e) {
 			return 2;
-		}finally {
+		} finally {
 			return 3;
 		}
 	}
