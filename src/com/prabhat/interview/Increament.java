9e7d6f7038a4de3db93420af3b66115cd824b56f
Prabhat11 <prabhat11.ranjan@gmail.com>
2018-03-01 11:56:13 +0530

    Core Java Basic Questions

---
+++ src/com/prabhat/interview/Increament.java
@@ -0,0 +1,18 @@
+package com.prabhat.interview;
+
+public class Increament {
+	
+	public void preIncreament(){
+		int i=0;
+		i++;
+		System.out.println(i);
+		i=++i;
+		System.out.println(i);
+
+	}
+   public static void main(String[] args) {
+	Increament t=new Increament();
+	t.preIncreament();
+	}
+}
+   
---
+++ src/com/prabhat/interview/TestTryCase.java
@@ -0,0 +1,20 @@
+package com.prabhat.interview;
+
+public class TestTryCase {
+	
+	public static void main(String[] args) {
+		
+		TestTryCase t=new TestTryCase();
+		System.out.println(t.getNum());
+	}
+	
+	public int getNum(){
+		try{
+			return 1;
+		}catch(Exception e){
+			return 2;
+		}finally {
+			return 3;
+		}
+	}
+}
