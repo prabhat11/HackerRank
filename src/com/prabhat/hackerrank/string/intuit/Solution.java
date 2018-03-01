2bc668cd6713a81b9993d4af0bbdf5c9f980501f
Prabhat11 <prabhat11.ranjan@gmail.com>
2018-03-01 10:30:00 +0530

    first commit of hacker rank practice

---
+++ src/com/prabhat/hackerrank/string/intuit/Solution.java
@@ -0,0 +1,16 @@
+package com.prabhat.hackerrank.string.intuit;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	static String[] getMovieTitle(){
+		
+		return null;
+		
+	}
+
+}
