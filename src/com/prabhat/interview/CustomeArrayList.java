3682aa16cfeb82c28bde36902603859e4b9d69c6
Prabhat11 <prabhat11.ranjan@gmail.com>
2018-03-01 15:05:54 +0530

    some document added

---
+++ src/com/prabhat/interview/CustomeArrayList.java
@@ -0,0 +1,14 @@
+package com.prabhat.interview;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CustomeArrayList {
+	Set<> set=new HashSet<>();
+	
+	public void setData(){
+		
+	}
+	
+
+}
